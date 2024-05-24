2edda8cdcb6a37a45594e1dedd82ff04d2781caf
Hadid Ramadhan <137972935+InitialH14@users.noreply.github.com>
2024-05-24 11:35:25 +0700

    Update solo_scrapping.py

--- src/solo_scrapping.py
+++ src/solo_scrapping.py
@@ -8,7 +8,7 @@ opsi = webdriver.ChromeOptions()
 servis = Service('chromedriver.exe')
 driver = webdriver.Chrome(service=servis, options=opsi)
 
-varsity_link = "https://www.tokopedia.com/discovery/deals?activeTab=1&rpc_sortfilter_category_id=60&rpc_sortfilter_ob=25"
+pakaian_link = "ISI_LINK_SETELAH_SEARCH"
 
 driver.set_window_size(1300, 800)
 driver.get(varsity_link)
@@ -23,14 +23,14 @@ data = BeautifulSoup(content, 'html.parser')
 
 list_nama, list_harga, list_penjualan, list_diskon, list_lokasi = [],[],[],[],[]
 i=1
-for area in data.find_all('div', class_="UszHKI"):
+for area in data.find_all('div', class_="GANTI_CLASS_BOX"):
     print(i)
     print("Mengambil data ke-", i)
-    nama = area.find('div', class_="prd_link-product-name css-3um8ox").get_text()
-    harga = area.find('div', class_="prd_link-product-price css-h66vau").get_text()
-    terjual = area.find('span', class_="prd_label-integrity css-1sgek4h").get_text()
-    diskon = area.find('div', class_="prd_badge-product-discount css-1xelcdh").get_text()
-    lokasi_toko = area.find('span', class_="prd_link-shop-loc css-1kdc32b").get_text()
+    nama = area.find('div', class_="GANTI_CLASS_YANG_SESUAI").get_text()
+    harga = area.find('div', class_="GANTI_CLASS_YANG_SESUAI").get_text()
+    terjual = area.find('span', class_="GANTI_CLASS_YANG_SESUAI").get_text()
+    diskon = area.find('div', class_="GANTI_CLASS_YANG_SESUAI").get_text()
+    lokasi_toko = area.find('span', class_="GANTI_CLASS_YANG_SESUAI").get_text()
 
     list_nama.append(nama)
     list_harga.append(harga)
@@ -41,4 +41,4 @@ for area in data.find_all('div', class_="UszHKI"):
     print("---------------")
 
 df = pd.DataFrame({'Nama' : list_nama, 'Harga': list_harga, 'Terjual': list_penjualan, 'Diskon': list_diskon, 'Lokasi Toko': list_lokasi})
-df.to_csv('computer.csv', index=False)
+df.to_csv('NAMA_FILE.csv', index=False)
